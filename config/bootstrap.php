502a84a3eba182c6f3e163ec4031131ea4133fd4
niel <niel.archer@gmail.com>
2016-03-22 13:06:11 +0000

    Refactor filter for Dispatchers to use lithium\aop\Filters during bootstrap.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,26 +3,28 @@
 /**
  * Initialize code index.
  */
+use lithium\aop\Filters;
 use lithium\core\Libraries;
-use lithium\action\Dispatcher;
-use lithium\console\Dispatcher as ConsoleDispatcher;
 use li3_docs\extensions\docs\Code;
 
-$filter = function($self, $params, $chain) {
-	$indexPath = Libraries::get(true, 'path') . '/resources/docs.index.json';
+$filter = function ($params, $next) {
+		$indexPath = Libraries::get(true, 'path') . '/resources/docs.index.json';
 
-	if (file_exists($indexPath) && is_readable($indexPath)) {
-		Code::index((array) json_decode(file_get_contents($indexPath), true));
-	}
-	$result = $chain->next($self, $params, $chain);
+		if (file_exists($indexPath) && is_readable($indexPath)) {
+			Code::index((array)json_decode(file_get_contents($indexPath), true));
+		}
+		$result = $next($params);
 
-	if (($index = Code::index()) && is_array($index) && is_writable(dirname($indexPath))) {
-		file_put_contents($indexPath, json_encode($index));
-	}
-	return $result;
-};
-Dispatcher::applyFilter('run', $filter);
-ConsoleDispatcher::applyFilter('run', $filter);
+		if (($index = Code::index()) && is_array($index) && is_writable(dirname($indexPath))) {
+			file_put_contents($indexPath, json_encode($index));
+		}
+
+		return $result;
+	};
+
+
+Filters::apply('lithium\action\Dispatcher', 'run', $filter);
+Filters::apply('lithium\console\Dispatcher', 'run', $filter);
 
 /**
  * Setup default options:
