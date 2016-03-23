1a622fac00106c687f054df43df49fcefca5811f
niel <niel.archer@gmail.com>
2016-03-23 03:06:16 +0000

    Correct code style

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,20 +8,19 @@ use lithium\core\Libraries;
 use li3_docs\extensions\docs\Code;
 
 $filter = function ($params, $next) {
-		$indexPath = Libraries::get(true, 'path') . '/resources/docs.index.json';
+	$indexPath = Libraries::get(true, 'path') . '/resources/docs.index.json';
 
-		if (file_exists($indexPath) && is_readable($indexPath)) {
-			Code::index((array)json_decode(file_get_contents($indexPath), true));
-		}
-		$result = $next($params);
+	if (file_exists($indexPath) && is_readable($indexPath)) {
+		Code::index((array)json_decode(file_get_contents($indexPath), true));
+	}
+	$result = $next($params);
 
-		if (($index = Code::index()) && is_array($index) && is_writable(dirname($indexPath))) {
-			file_put_contents($indexPath, json_encode($index));
-		}
+	if (($index = Code::index()) && is_array($index) && is_writable(dirname($indexPath))) {
+		file_put_contents($indexPath, json_encode($index));
+	}
 
-		return $result;
-	};
-
+	return $result;
+};
 
 Filters::apply('lithium\action\Dispatcher', 'run', $filter);
 Filters::apply('lithium\console\Dispatcher', 'run', $filter);
